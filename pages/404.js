7431c63775e887fb79c9bfffb846fec79590dc06
emmpii8 <42419839+emmpii8@users.noreply.github.com>
2021-08-10 11:05:02 +0100

    redirects

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,15 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 const NotFound = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 3000);
+    }, [])
+
     return ( <div className="not-found">
         <h1>Ooooops...</h1>
         <h2>That page cannot be found.</h2>
