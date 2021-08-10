adcaeab3ef9aa284aed3bc0c7e4523fea4913a51
emmpii8 <42419839+emmpii8@users.noreply.github.com>
2021-08-10 15:52:13 +0100

    images, metadata and fetching data

--- pages/about.js
+++ pages/about.js
@@ -1,38 +1,44 @@
+import Head from 'next/head';
 const About = () => {
-    return (
+  return (
+    <>
+      <Head>
+        <title>About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
       <div>
         <h1>About</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ipsum
-          id consectetur! Sint aliquam adipisci architecto nulla iure totam
-          voluptates accusantium iste mollitia accusamus quibusdam culpa
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          ipsum id consectetur! Sint aliquam adipisci architecto nulla iure
+          totam voluptates accusantium iste mollitia accusamus quibusdam culpa
           laboriosam ipsa ipsam optio debitis inventore, fugiat odio a libero,
-          illum alias distinctio. Illo tenetur quam animi, dolor autem asperiores
-          facilis molestiae! Veritatis consectetur dolore ducimus impedit animi
-          deleniti magnam labore? Accusamus, distinctio ad porro unde nostrum
-          laboriosam excepturi ipsam praesentium qui vitae? Laudantium culpa vero
-          alias, explicabo quis amet iusto incidunt temporibus consequuntur
-          cupiditate, inventore optio error. Ducimus molestias tenetur, aperiam
-          dolorem repellendus animi temporibus earum odit dolores ullam, deleniti
-          explicabo reprehenderit suscipit.
+          illum alias distinctio. Illo tenetur quam animi, dolor autem
+          asperiores facilis molestiae! Veritatis consectetur dolore ducimus
+          impedit animi deleniti magnam labore? Accusamus, distinctio ad porro
+          unde nostrum laboriosam excepturi ipsam praesentium qui vitae?
+          Laudantium culpa vero alias, explicabo quis amet iusto incidunt
+          temporibus consequuntur cupiditate, inventore optio error. Ducimus
+          molestias tenetur, aperiam dolorem repellendus animi temporibus earum
+          odit dolores ullam, deleniti explicabo reprehenderit suscipit.
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ipsum
-          id consectetur! Sint aliquam adipisci architecto nulla iure totam
-          voluptates accusantium iste mollitia accusamus quibusdam culpa
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          ipsum id consectetur! Sint aliquam adipisci architecto nulla iure
+          totam voluptates accusantium iste mollitia accusamus quibusdam culpa
           laboriosam ipsa ipsam optio debitis inventore, fugiat odio a libero,
-          illum alias distinctio. Illo tenetur quam animi, dolor autem asperiores
-          facilis molestiae! Veritatis consectetur dolore ducimus impedit animi
-          deleniti magnam labore? Accusamus, distinctio ad porro unde nostrum
-          laboriosam excepturi ipsam praesentium qui vitae? Laudantium culpa vero
-          alias, explicabo quis amet iusto incidunt temporibus consequuntur
-          cupiditate, inventore optio error. Ducimus molestias tenetur, aperiam
-          dolorem repellendus animi temporibus earum odit dolores ullam, deleniti
-          explicabo reprehenderit suscipit.
+          illum alias distinctio. Illo tenetur quam animi, dolor autem
+          asperiores facilis molestiae! Veritatis consectetur dolore ducimus
+          impedit animi deleniti magnam labore? Accusamus, distinctio ad porro
+          unde nostrum laboriosam excepturi ipsam praesentium qui vitae?
+          Laudantium culpa vero alias, explicabo quis amet iusto incidunt
+          temporibus consequuntur cupiditate, inventore optio error. Ducimus
+          molestias tenetur, aperiam dolorem repellendus animi temporibus earum
+          odit dolores ullam, deleniti explicabo reprehenderit suscipit.
         </p>
       </div>
-    );
-  };
-  
-  export default About;
-  
+    </>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -7,27 +7,33 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum odit
-        architecto commodi eius blanditiis voluptates libero nesciunt, hic
-        officiis amet distinctio beatae, obcaecati enim dolor impedit corrupti
-        modi ratione aliquid assumenda error quia eaque voluptatibus adipisci
-        id. Laudantium obcaecati commodi ducimus dolorem error, doloribus ea,
-        dolorum architecto, magni molestias expedita!
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum odit
-        architecto commodi eius blanditiis voluptates libero nesciunt, hic
-        officiis amet distinctio beatae, obcaecati enim dolor impedit corrupti
-        modi ratione aliquid assumenda error quia eaque voluptatibus adipisci
-        id. Laudantium obcaecati commodi ducimus dolorem error, doloribus ea,
-        dolorum architecto, magni molestias expedita!
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn} >See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum odit
+          architecto commodi eius blanditiis voluptates libero nesciunt, hic
+          officiis amet distinctio beatae, obcaecati enim dolor impedit corrupti
+          modi ratione aliquid assumenda error quia eaque voluptatibus adipisci
+          id. Laudantium obcaecati commodi ducimus dolorem error, doloribus ea,
+          dolorum architecto, magni molestias expedita!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum odit
+          architecto commodi eius blanditiis voluptates libero nesciunt, hic
+          officiis amet distinctio beatae, obcaecati enim dolor impedit corrupti
+          modi ratione aliquid assumenda error quia eaque voluptatibus adipisci
+          id. Laudantium obcaecati commodi ducimus dolorem error, doloribus ea,
+          dolorum architecto, magni molestias expedita!
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
