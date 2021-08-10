828fa5e506a2df36602a7897e800df23a9b621b1
emmpii8 <42419839+emmpii8@users.noreply.github.com>
2021-08-10 10:20:38 +0100

    components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum odit
@@ -22,6 +25,7 @@ export default function Home() {
         id. Laudantium obcaecati commodi ducimus dolorem error, doloribus ea,
         dolorum architecto, magni molestias expedita!
       </p>
+      <Footer />
     </div>
   );
 }
