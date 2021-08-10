43ae588cab0bcdad80350281b0a86caf6eabfee1
emmpii8 <42419839+emmpii8@users.noreply.github.com>
2021-08-10 16:06:26 +0100

    dynamic routing 2

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,9 +1,25 @@
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  const paths = data.map((ninja) => {
+    return {
+      parms: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
-    return ( 
-        <div>
-            <h1>Details Page</h1>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <h1>Details Page</h1>
+    </div>
+  );
+};
+
 export default Details;
