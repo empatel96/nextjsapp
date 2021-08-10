3cc69bb52f81ced951887df0710509a6eb45dc41
emmpii8 <42419839+emmpii8@users.noreply.github.com>
2021-08-10 09:55:38 +0100

    fix
    added content

---
+++ pages/about.js
@@ -0,0 +1,38 @@
+const About = () => {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ipsum
+          id consectetur! Sint aliquam adipisci architecto nulla iure totam
+          voluptates accusantium iste mollitia accusamus quibusdam culpa
+          laboriosam ipsa ipsam optio debitis inventore, fugiat odio a libero,
+          illum alias distinctio. Illo tenetur quam animi, dolor autem asperiores
+          facilis molestiae! Veritatis consectetur dolore ducimus impedit animi
+          deleniti magnam labore? Accusamus, distinctio ad porro unde nostrum
+          laboriosam excepturi ipsam praesentium qui vitae? Laudantium culpa vero
+          alias, explicabo quis amet iusto incidunt temporibus consequuntur
+          cupiditate, inventore optio error. Ducimus molestias tenetur, aperiam
+          dolorem repellendus animi temporibus earum odit dolores ullam, deleniti
+          explicabo reprehenderit suscipit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ipsum
+          id consectetur! Sint aliquam adipisci architecto nulla iure totam
+          voluptates accusantium iste mollitia accusamus quibusdam culpa
+          laboriosam ipsa ipsam optio debitis inventore, fugiat odio a libero,
+          illum alias distinctio. Illo tenetur quam animi, dolor autem asperiores
+          facilis molestiae! Veritatis consectetur dolore ducimus impedit animi
+          deleniti magnam labore? Accusamus, distinctio ad porro unde nostrum
+          laboriosam excepturi ipsam praesentium qui vitae? Laudantium culpa vero
+          alias, explicabo quis amet iusto incidunt temporibus consequuntur
+          cupiditate, inventore optio error. Ducimus molestias tenetur, aperiam
+          dolorem repellendus animi temporibus earum odit dolores ullam, deleniti
+          explicabo reprehenderit suscipit.
+        </p>
+      </div>
+    );
+  };
+  
+  export default About;
+  
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum odit
+        architecto commodi eius blanditiis voluptates libero nesciunt, hic
+        officiis amet distinctio beatae, obcaecati enim dolor impedit corrupti
+        modi ratione aliquid assumenda error quia eaque voluptatibus adipisci
+        id. Laudantium obcaecati commodi ducimus dolorem error, doloribus ea,
+        dolorum architecto, magni molestias expedita!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum odit
+        architecto commodi eius blanditiis voluptates libero nesciunt, hic
+        officiis amet distinctio beatae, obcaecati enim dolor impedit corrupti
+        modi ratione aliquid assumenda error quia eaque voluptatibus adipisci
+        id. Laudantium obcaecati commodi ducimus dolorem error, doloribus ea,
+        dolorum architecto, magni molestias expedita!
+      </p>
     </div>
-  )
+  );
 }
