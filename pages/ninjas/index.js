98a47955cc980a7192e81265fcd4b6f1ab1c1605
emmpii8 <42419839+emmpii8@users.noreply.github.com>
2021-08-10 15:58:47 +0100

    dynamic routing 1

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,24 +1,25 @@
-import styles from '../../styles/Ninjas.module.css'
+import Link from "next/link";
+import styles from "../../styles/Ninjas.module.css";
 
 export const getStaticProps = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
   return {
-    props: { ninjas: data}
-  }
+    props: { ninjas: data },
+  };
 };
 
 const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
-      {ninjas.map(ninja => (
-        <div key={ninja.id}>
+      {ninjas.map((ninja) => (
+        <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
           <a className={styles.single}>
-            <h3>{ ninja.name }</h3>
+            <h3>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
