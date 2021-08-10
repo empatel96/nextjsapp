66ba06b2d43d16f7ecf45e54d55d85e5a49dd302
emmpii8 <42419839+emmpii8@users.noreply.github.com>
2021-08-10 11:01:14 +0100

    error page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+const NotFound = () => {
+    return ( <div className="not-found">
+        <h1>Ooooops...</h1>
+        <h2>That page cannot be found.</h2>
+        <p>Go back to the <Link href="/"><a>Home Page</a></Link></p>
+        
+    </div> );
+}
+ 
+export default NotFound;
